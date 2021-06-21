1bab6bc660cf25d2676232ae0ea545bab16624c6
Jeisson Salamanca <jeissonssp@gmail.com>
2021-06-21 15:21:24 -0500

    Peticion Post en obra

--- src/components/holandAuction.jsx
+++ src/components/holandAuction.jsx
@@ -1,21 +1,40 @@
 import React, { useState, useEffect } from "react";
 
-import { getAuctions } from "../utils/request";
+import { getAuctions, addAuction } from "../utils/request";
 
 const HolandAuction = () => {
   const [data, setData] = useState([]);
   const auction_id = "5cc18be54d4d412bb9e0209b9d6aa4f0";
-
+  const request = {
+    "datos": {
+      "title": "carcasas para premios estatales",
+      "valor": {
+        "cantidad": 500
+      },
+      "minimalStep": {
+        "cantidad": 35
+      },
+      "artículos": [
+        {
+          "description": "carcasas para premios estatales",
+          "cantidad": 5
+        }
+      ],
+      "tenderPeriod": {},
+      "provisionMethodType": "información privilegiada"
+    }
+  }
   useEffect(() => {
-    getAuctions(auction_id).then((data) => {
-      setData(data.data.data);
-      console.log(data);
+    getAuctions(auction_id).then((response) => {
+      setData(response);
+    });
+    addAuction(request, auction_id).then((response) => {
+      console.log(response);
     });
   }, []);
   return (
   <div>
     <h1>Holand Auction</h1>
-    {data.auctionID}
     </div>
     );
 };
--- src/utils/request.jsx
+++ src/utils/request.jsx
@@ -4,15 +4,18 @@ const KEY = "ba417ff3a1a446de9b7207b0e8f1ceaf";
 
 // https://api.atreus.auction/api/2.4/auctions/5cc18be54d4d412bb9e0209b9d6aa4f0?acc_token=ba417ff3a1a446de9b7207b0e8f1ceaf
 
-const instance = axios.create({
+const instanceGet = axios.create({
   baseURL: "https://api.atreus.auction/api/2.4/auctions/",
+});
+const instancePost = axios.create({
+  baseURL: "api.atreus.auction/api/2.4/auctions/",
 });
 
 
 export const getAuctions = async (auction_id) => {
   try {
     
-    const data = await instance.get(`${auction_id}?acc_token=${KEY}`);
+    const data = await instanceGet.get(`${auction_id}?acc_token=${KEY}`);
     return data;
   } catch (error) {
     if (error) {
@@ -23,7 +26,7 @@ export const getAuctions = async (auction_id) => {
 export const addAuction = async (request,auction_id)=> {
   
   try {
-     const data = await instance.post(
+     const data = await instancePost.post(
       `${auction_id}`, request
      );
      return data;
